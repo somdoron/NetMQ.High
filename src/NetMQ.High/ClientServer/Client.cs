f360746583b5554e00bc51968f24dbf1782bdf39
somdoron <somdoron@gmail.com>
2015-12-14 12:02:17 +0200

    move subclasses to own class

--- src/NetMQ.High/ClientServer/Client.cs
+++ src/NetMQ.High/ClientServer/Client.cs
@@ -1,138 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using NetMQ.High.Serializers;
-using NetMQ.Sockets;
 
 namespace NetMQ.High.ClientServer
 {
     public class Client : IDisposable
-    {        
-        class Actor : IShimHandler
-        {
-            private readonly ISerializer m_serializer;
-            private readonly NetMQQueue<OutgoingMessage> m_outgoingQueue;
-            private readonly string m_address;
-
-            private DealerSocket m_clientSocket;
-            private PairSocket m_shim;
-            private Poller m_poller;
-
-            private Codec m_codec;
-
-            private UInt64 m_nextMessageId;
-
-            private Dictionary<UInt64, OutgoingMessage> m_pendingRequests; 
-
-            public Actor(ISerializer serializer, NetMQQueue<OutgoingMessage> outgoingQueue, string address)
-            {
-                m_serializer = serializer;
-                m_outgoingQueue = outgoingQueue;
-                m_address = address;
-                m_codec = new Codec();
-                m_nextMessageId = 0;
-                m_pendingRequests = new Dictionary<ulong, OutgoingMessage>();
-            }
-
-            public void Run(PairSocket shim)
-            {
-                m_poller = new Poller();
-
-                m_shim = shim;
-                m_shim.ReceiveReady += OnShimReady;
-                m_poller.AddSocket(m_shim);
-
-                m_clientSocket = Global.Context.CreateDealerSocket();
-                m_clientSocket.Connect(m_address);
-                m_clientSocket.ReceiveReady += OnClientReady;
-                m_poller.AddSocket(m_clientSocket);
-
-                m_outgoingQueue.ReceiveReady += OnOutgoingQueueReady;
-                m_poller.AddSocket(m_outgoingQueue);                
-             
-                m_shim.SignalOK();
-                m_poller.PollTillCancelled();
-
-                m_clientSocket.Dispose();                            
-            }
-
-            private void OnShimReady(object sender, NetMQSocketEventArgs e)
-            {
-                string command = m_shim.ReceiveFrameString();
-
-                if (command == NetMQActor.EndShimMessage)
-                    m_poller.Cancel();                                
-            }
-
-            private void OnOutgoingQueueReady(object sender, NetMQQueueEventArgs<OutgoingMessage> e)
-            {
-                var outgoingMessage = m_outgoingQueue.Dequeue();
-                
-                // TODO: Zproto should support ArraySegment to improve performance            
-                var bodySegment = m_serializer.Serialize(outgoingMessage.Message);
-                byte[] body = new byte[bodySegment.Count];
-                Buffer.BlockCopy(bodySegment.Array, bodySegment.Offset, body, 0, bodySegment.Count);
-
-                UInt64 messageId = m_nextMessageId++;
-
-                string subject = m_serializer.GetObjectSubject(outgoingMessage.Message);
-
-                m_codec.Id = Codec.MessageId.Message;
-                m_codec.Message.MessageId = messageId;
-                m_codec.Message.Service = outgoingMessage.Service;
-                m_codec.Message.Subject = subject;
-                m_codec.Message.Body = body;
-
-                // one way message
-                if (outgoingMessage.Oneway)
-                {
-                    m_codec.Message.OneWay = 1;                                      
-                }
-                else
-                {
-                    m_codec.Message.OneWay = 0;
-                                                                                        
-                    // add to pending requests dictionary
-                    // TODO: we might want to create a pending message structure that will not hold reference to the message (can lead to GC second generation)
-                    m_pendingRequests.Add(messageId, outgoingMessage);                   
-                }
-
-                m_codec.Send(m_clientSocket);
-            }
-
-            private void OnClientReady(object sender, NetMQSocketEventArgs e)
-            {
-                m_codec.Receive(m_clientSocket);
-                
-                OutgoingMessage outgoingMessage;
-
-                UInt64 relatedMessageId = m_codec.Id == Codec.MessageId.Message
-                    ? m_codec.Message.RelatedMessageId
-                    : m_codec.Error.RelatedMessageId;
-
-                if (m_pendingRequests.TryGetValue(relatedMessageId, out outgoingMessage))
-                {                    
-                    if (m_codec.Id == Codec.MessageId.Message)
-                    {                        
-                        var body = m_serializer.Deserialize(m_codec.Message.Subject, m_codec.Message.Body, 0,
-                            m_codec.Message.Body.Length);
-                        outgoingMessage.SetResult(body);
-                    }
-                    else
-                    {
-                        // TODO: we should pass more meaningful exceptions
-                        outgoingMessage.SetException(new Exception());
-                    }
-                }                
-                else
-                {
-                    Debug.Assert(false, "Response doesn't match any request");
-                }
-            }         
-        }
-        
+    {
         private NetMQActor m_actor;
         private NetMQQueue<OutgoingMessage> m_outgoingQueue;
 
@@ -144,7 +18,7 @@ namespace NetMQ.High.ClientServer
         public Client(ISerializer serializer, string address)
         {
             m_outgoingQueue = new NetMQQueue<OutgoingMessage>(Global.Context);
-            m_actor = NetMQActor.Create(Global.Context, new Actor(serializer, m_outgoingQueue, address));            
+            m_actor = NetMQActor.Create(Global.Context, new ClientEngine(serializer, m_outgoingQueue, address));            
         }
 
         /// <summary>
--- src/NetMQ.High/ClientServer/Server.cs
+++ src/NetMQ.High/ClientServer/Server.cs
@@ -1,144 +1,11 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Threading;
-using System.Threading.Tasks;
 using NetMQ.High.Serializers;
-using NetMQ.Sockets;
 
 namespace NetMQ.High.ClientServer
 {
     public class Server : IDisposable
-    {
-        private const string BindCommand = "BIND";        
-
-        class Actor : IShimHandler
-        {
-            private readonly ISerializer m_serializer;
-            private readonly IServerHandler m_handler;
-
-            private Poller m_poller;
-            private RouterSocket m_serverSocket;
-            private PairSocket m_shim;
-            private NetMQScheduler m_scheduler;
-            private Codec m_codec;
-
-            public Actor(ISerializer serializer, IServerHandler handler)
-            {
-                m_serializer = serializer;
-                m_handler = handler;
-                m_codec = new Codec();
-            }
-
-            public void Run(PairSocket shim)
-            {
-                m_poller = new Poller();
-
-                m_serverSocket = Global.Context.CreateRouterSocket();
-                m_serverSocket.ReceiveReady += OnServerReady;
-                m_poller.AddSocket(m_serverSocket);
-
-                m_shim = shim;
-                m_shim.ReceiveReady += OnShimReady;
-                m_poller.AddSocket(m_shim);
-
-                // used to make sure continuation of tasks run on this thread
-                m_scheduler = new NetMQScheduler(Global.Context, m_poller);
-
-                m_shim.SignalOK();
-                m_poller.PollTillCancelled();
-                m_scheduler.Dispose();
-                m_serverSocket.Dispose();
-            }
-
-            private void OnShimReady(object sender, NetMQSocketEventArgs e)
-            {
-                string command = m_shim.ReceiveFrameString();
-
-                switch (command)
-                {
-                    case NetMQActor.EndShimMessage:
-                        m_poller.Cancel();
-                        break;
-                    case BindCommand:
-                        string addresss = m_shim.ReceiveFrameString();
-                        m_serverSocket.Bind(addresss);
-                        break;                    
-                }
-            }
-
-            private void OnServerReady(object sender, NetMQSocketEventArgs e)
-            {
-                m_codec.Receive(m_serverSocket);
-
-                // TODO: this should be manage with a dictionary, this is temporary hack
-                UInt32 connectionId = BitConverter.ToUInt32(m_codec.RoutingId, 1);
-
-                bool oneway = m_codec.Message.OneWay == 1;
-
-                object message = m_serializer.Deserialize(m_codec.Message.Subject, m_codec.Message.Body, 0, m_codec.Message.Body.Length);
-
-                ulong messageId = m_codec.Message.MessageId;
-                string service = m_codec.Message.Service;
-                string subject = m_codec.Message.Subject;
-                
-                if (oneway)
-                {                    
-                    // TODO: this should run on user provided task scheduler
-                    ThreadPool.QueueUserWorkItem(s => m_handler.HandleOneWay(messageId, connectionId, service, subject, message));
-                }
-                else
-                {                    
-                    // TODO: this should run on user provided task scheduler
-                    ThreadPool.QueueUserWorkItem(s =>
-                    {
-                        // we set the task scheduler so we now run on the actor thread to complete the request async
-                        m_handler.HandleRequestAsync(messageId, connectionId, service, subject, message).
-                            ContinueWith(t => CompleteRequestAsync(t, messageId, connectionId), m_scheduler);
-                    });
-                }
-            }
-
-            private byte[] ConvertConnectionIdToRoutingId(uint connectionId)
-            {
-                byte[] routingId = new byte[5];
-                Buffer.BlockCopy(BitConverter.GetBytes(connectionId), 0, routingId, 1, 4);
-
-                return routingId;
-            }
-
-            private void CompleteRequestAsync(Task<Object> t, ulong originMessageId, uint originConnectionId)
-            {
-                if (t.IsFaulted)
-                {
-                    // Exception, let just send an error
-                    m_codec.Id = Codec.MessageId.Message;
-                    m_codec.Error.RelatedMessageId = originMessageId;
-
-                    m_codec.RoutingId = ConvertConnectionIdToRoutingId(originConnectionId);
-                    m_codec.Send(m_serverSocket);
-                }
-                else
-                {
-                    var reply = t.Result;
-
-                    string subject = m_serializer.GetObjectSubject(reply);
-
-                    // TODO: Zproto should support ArraySegment to improve performance            
-                    var bodySegment = m_serializer.Serialize(reply);
-                    byte[] body = new byte[bodySegment.Count];
-                    Buffer.BlockCopy(bodySegment.Array, bodySegment.Offset, body, 0, bodySegment.Count);
-
-                    m_codec.Id = Codec.MessageId.Message;
-                    m_codec.Message.Subject = subject;
-                    m_codec.Message.Body = body;
-                    m_codec.Message.RelatedMessageId = originMessageId;
-
-                    m_codec.RoutingId = ConvertConnectionIdToRoutingId(originConnectionId);
-                    m_codec.Send(m_serverSocket);
-                }
-            }
-        }
-
+    {        
         private NetMQActor m_actor;
 
         /// <summary>
@@ -157,7 +24,7 @@ namespace NetMQ.High.ClientServer
         /// <param name="handler">Handler to handle messages from client</param>
         public Server(ISerializer serializer, IServerHandler handler)
         {
-            m_actor = NetMQActor.Create(Global.Context, new Actor(serializer, handler));
+            m_actor = NetMQActor.Create(Global.Context, new ServerEngine(serializer, handler));
         }
 
         /// <summary>
@@ -168,7 +35,7 @@ namespace NetMQ.High.ClientServer
         {
             lock (m_actor)
             {
-                m_actor.SendMoreFrame(BindCommand).SendFrame(address);
+                m_actor.SendMoreFrame(ServerEngine.BindCommand).SendFrame(address);
             }
         }
         
